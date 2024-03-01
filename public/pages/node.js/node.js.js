db5b9542e48d8a204a18bf789c4cf0bb4ec922c8
Wei Yang <weix0015@stud.kea.dk>
2024-03-01 20:49:19 +0100

    added documentation for node.js

---
+++ public/pages/node.js/node.js.js
@@ -0,0 +1,6 @@
+const nodejs_image = document.getElementById("nodejs_image");
+nodejs_image.addEventListener("click", nodejs_website);
+
+function nodejs_website() {
+  window.open("https://nodejs.org/en");
+}
